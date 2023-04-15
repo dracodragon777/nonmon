76a2be9d6027c350d7b067bd3365814367582623
NEJ <dejulnes@aol.com>
2023-04-14 17:29:22 -0700

    Create Matrix.ts

---
+++ Matrix.ts
@@ -0,0 +1,37 @@
+class Matrix {
+    protected _columns: Vector[];
+    constructor(columns: Vector[]) {
+        this._columns = columns;
+    }
+    get columns() {
+        return this._columns;
+    }
+
+    public rows() {
+        const matrix = this.columns[0].components.map((_, column) => this.columns.map(row => row.components[column]))
+        return new Matrix(matrix.map(vector => new Vector(vector)));
+    }
+    public transpose() {
+        return new Matrix(this.rows().columns)
+    }
+    protected componentWiseOperation(func: Function, columns: Vector[]) {
+        columns.map((column, index) => column.components.map((row, i) => func(this.columns[i].components[index], row)))
+    }
+    public add(other: Matrix) {
+        return this.componentWiseOperation((ac: number, bc: number) => ac + bc, other.columns);
+    }
+    public subtract(other: Matrix) { 
+        return this.componentWiseOperation((ac: number, bc: number) => ac - bc, other.columns);
+    }
+    public scalar(scale: number) {
+        return new Matrix(this.columns.map(column => new Vector(column.components.map(component => component * scale))));
+    }
+    public toString() {
+        let out = ''
+        this.columns.forEach((vector, index) => {
+            if (vector.equalTo(this.columns[this.columns.length - 1])) out += `${index}: ${vector.toString()}`
+            else out += `${index}: ${vector.toString()}, `
+        })
+        return out;
+    }
+}
