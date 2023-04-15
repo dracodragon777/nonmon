eaa8711ba81f52e8cb6fdf93e6f0dbe70c8bde04
NEJ <dejulnes@aol.com>
2023-04-14 18:47:16 -0700

    Update Voronoi.ts

--- Voronoi.ts
+++ Voronoi.ts
@@ -25,6 +25,78 @@ class Edge {
     }
 }
 
+class Triangle {
+    protected _vertices: [Point, Point, Point];
+    protected _edges: [Edge, Edge, Edge];
+    constructor(a: Point, b: Point, c: Point) {
+        this._vertices = [a, b, c];
+        this._edges = [new Edge(a, b), new Edge(b, c), new Edge(c, a)]
+    }
+
+    get edges() { return this._edges; }
+    get vertices() { return this._vertices; }
+
+    get a() { return this._vertices[0]; }
+    get b() { return this._vertices[1]; }
+    get c() { return this._vertices[2]; }
+
+    get ab() { return this._edges[0]; }
+    get bc() { return this._edges[1]; }
+    get ca() { return this._edges[2]; }
+
+    public move(vector: Point) {
+        const vertices = this.vertices.map((v, i) => {
+            if(v instanceof Vector2 && vector instanceof Vector2) return v.add(vector)
+            else if(v instanceof Vector3 && vector instanceof Vector3) return v.add(vector)
+            else return null;
+        });
+        return new Triangle(vertices[0], vertices[1], vertices[2]);
+    }
+
+    containsPoint(point: Point): boolean {
+        const [a, b, c] = this.vertices;
+        const det = ((b.components[1] - c.components[1]) * (a.components[0] - c.components[0]) +
+            (c.components[0] - b.components[0]) * (a.components[1] - c.components[1]));
+
+        const alpha = ((b.components[1] - c.components[1]) * (point.components[0] - c.components[0]) +
+            (c.components[0] - b.components[0]) * (point.components[1] - c.components[1])) / det;
+        const beta = ((c.components[1] - a.components[1]) * (point.components[0] - c.components[0]) +
+            (a.components[0] - c.components[0]) * (point.components[1] - c.components[1])) / det;
+        const gamma = 1 - alpha - beta;
+
+        return alpha >= 0 && alpha <= 1 && beta >= 0 && beta <= 1 && gamma >= 0 && gamma <= 1;
+    }
+    sharesEdge(other: Triangle): boolean {
+        const sharedVertices = this.vertices.filter(vertex => other.vertices.includes(vertex));
+        return sharedVertices.length === 2;
+    }
+
+    circumcircleContains(point: Point): boolean {
+        const [a, b, c] = this.vertices;
+        const circleCenter = VectorFunctions.circleCenter(new Edge(a, b), new Edge(b, c));
+        const radiusSquared = circleCenter.distanceToSquared(a);
+        const pointDistanceSquared = circleCenter.distanceToSquared(point);
+        return pointDistanceSquared <= radiusSquared;
+    }
+
+    hasVertex(vertex: Point): boolean {
+        return this.vertices.includes(vertex);
+    }
+
+    isNeighbor(other: Triangle): boolean {
+        const sharedVertices = this.vertices.filter(vertex => other.vertices.includes(vertex));
+        return sharedVertices.length === 2;
+    }
+
+    isDelaunayForPoint(point: Point): boolean {
+        const [a, b, c] = this.vertices;
+        const circleCenter = VectorFunctions.circleCenter(new Edge(a, b), new Edge(b, c));
+        const radiusSquared = circleCenter.distanceToSquared(a);
+        const pointDistanceSquared = circleCenter.distanceToSquared(point);
+        return pointDistanceSquared >= radiusSquared;
+    }
+}
+
 class Cell {
     edges: Edge[];
     site: Site;
